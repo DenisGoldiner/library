b8ef23be343a3e2a90aefb0e4bb7acb1c1845da8
DenisGoldiner <goldiner19@gmail.com>
2016-01-10 21:49:35 +0200

    commit

--- com.denis/src/views/404_view.php
+++ com.denis/src/views/404_view.php
@@ -1,7 +1,16 @@
-<h1>404</h1>
-<p>
-    <!--
-    <img src="/images/404.png">
-    -->
-    404 NOT FOUND
-</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Фон</title>
+    <style>
+        body {
+            background: #c7b39b url(<?=WEB_PATH?>template/images/404.jpg); /* Цвет фона и путь к файлу */
+            color: #fff; /* Цвет текста */
+        }
+    </style>
+</head>
+<body>
+<p>...</p>
+</body>
+</html>
